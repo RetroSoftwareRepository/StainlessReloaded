16f49ec0c27410fe30da3328cc2504e73d611a06
Alex McKeever <alex.mckeever@sbcglobal.net>
2019-10-25 21:43:22 -0400

    Add files via upload

---
+++ Website/doc/about_welcome.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+<title>Welcome</title>
+<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
+</head>
+
+<body style="margin:20px;">
+
+<div style=" background-color: #eee;
+-webkit-border-radius: 10px;
+border: 1px solid #000;
+padding: 10px;"><span style="font-family: 'LucidaGrande-Bold', 'Lucida Grande', 'Lucida Sans Unicode', sans-serif; font-size: 13px;">
+Welcome to Stainless</span></div>
+
+<div style="padding:10px">
+	<span style="font-family: 'LucidaGrande', 'Lucida Grande', 'Lucida Sans Unicode', sans-serif; font-size: 12px;">
+
+<p>Thanks for downloading <a href="http://www.stainlessapp.com">Stainless</a>, a multi-process browser alternative to Google Chrome.</p>
+
+<p>Each tab in Stainless runs independently in a separate process, which means:<br/>
+<ul>
+<li>Increased performance.<br /><i>Slow page loads (especially due to plugins such as Flash) can't affect the responsiveness of the browser or other tabs.</i> 
+</li><br /><li>Increased stability.<br /> <i>Crashed pages can't bring down the browser or other tabs.</i></li>
+<br /><li>Decreased memory usage.<br /> <i>Once a tab is closed, the memory for that session is completely freed to the system.</i></li></ul></p>
+
+	<p>Stainless started out as a technology demo to showcase my own multi-processing architecture in response to Google Chrome (Stainless 0.1 was released three weeks after Google released Chrome for Windows). Sensing an opportunity and inspired by a growing fanbase, I decided to craft Stainless into a full-fledged browser and work on features that I hadn't seen before in other browsers.</p>
+	<p>A prime example is <i>parallel sessions</i>, which allow you to log into a site using different credentials in separate tabs at the same time. This new technology is woven throughout Stainless, from the private cookie storage system, to <i>session-aware bookmarks</i> that remember the session in which they were saved. I still believe this is a true browser innovation (and I'd love to see this implemented in Chrome).
+	 You can check it out by selecting <i>New Single Session Tab</i> from the File menu.</p>
+	<p>Regrettably, over the past couple of years it's been impossible for me to keep working on Stainless and thus the <a href="https://github.com/mesadynamics/stainless">source code</a> for Stainless is now available under the MIT License for those who might be interested in continuing the project.</p>
+	
+</span>	
+</div>
+
+</body>
+</html>
